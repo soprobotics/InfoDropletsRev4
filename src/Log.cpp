4062b89b58a3d71410c674973fa6fb24ef586f4c
matyasbejo <bejomatyi@gmail.com>
2026-02-10 16:27:06 +0100

    added debug mode

--- src/Log.cpp
+++ src/Log.cpp
@@ -2,8 +2,16 @@
 #include "RadioFunctions/RadioFunctions.h"
 
 namespace Log {
+    bool isDebugEnabled = true;
+
     void Log(const String& message) {
         RadioFunctions::SendMessage("[LOG] " + message);
+    }
+
+    void Debug(const String& message) {
+        if(isDebugEnabled){
+            RadioFunctions::SendMessage("[DEBUG] " + message);
+        }
     }
 
     void Error(const String& message) {
--- src/Log.h
+++ src/Log.h
@@ -3,7 +3,11 @@
 #include <Arduino.h>
 
 namespace Log {
+    extern bool isDebugEnabled;
+
     void Log(const String& message);
+
+    void Debug(const String& message);
 
     void Error(const String& message);
 
